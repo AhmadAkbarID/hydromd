494bc5ad92c95224dd921363aacde21a24a3f0fa
IbraDecode <ibradecode@gmail.com>
2026-06-22 18:41:34 +0000

    fix: add native flow nodes to relayMessage

--- hydro.js
+++ hydro.js
@@ -1476,7 +1476,13 @@ buttons: [
 }), })}
 }}, {quoted: jm})
 await hydro.relayMessage(msg.key.remoteJid, msg.message, {
-messageId: msg.key.id
+messageId: msg.key.id,
+additionalNodes: [{
+tag: 'biz', attrs: {}, content: [{
+tag: 'interactive', attrs: { type: 'native_flow', v: '1' },
+content: [{ tag: 'native_flow', attrs: { v: '9', name: 'mixed' } }]
+}]
+}]
 })
 }
 
